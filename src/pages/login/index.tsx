8e2c060f1af6d30cf5aea549ee9f1b3427429078
LZY_Shrio <170280176@qq.com>
2021-11-27 16:38:40 +0800

    feat:login更新

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,15 @@
+import { history } from 'umi'
 import style from './index.less'
 
 export default function LoginPage() {
-
+  const insertValue = () => {
+    localStorage.setItem('login', 'true')
+    history.push('/overview')
+  }
   return (
     <div>
-      请在localStorage中添加login：true
+      <div> 请在localStorage中添加login：true</div>
+      <button onClick={insertValue}>点击注入灵魂即可登录！</button>
     </div>
-   )
+  )
 }
