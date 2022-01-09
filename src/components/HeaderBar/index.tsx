7266d459fc8a53b929801fcf0eb6c52f496f5802
LZY_Shrio <170280176@qq.com>
2022-01-09 12:57:09 +0800

    feat：header更新

--- src/components/HeaderBar/index.tsx
+++ src/components/HeaderBar/index.tsx
@@ -1,22 +1,36 @@
 import React from 'react'
 import { Avatar, Popover, Input } from 'antd'
+import { history } from 'umi'
+import { UserOutlined } from '@ant-design/icons'
 const { Search } = Input
 import style from './index.less'
 
-const HeaderBar: React.FC = () =>{
+const HeaderBar: React.FC = () => {
   const onSearch = (value: string) => console.log(value)
 
+  const handleLoginClick = () => {
+    history.push('/login')
+  }
   return (
     <div className={style.wrap}>
-      <Search
-        placeholder="牛皮程序员修炼指南？"
-        onSearch={onSearch}
-        enterButton
-        style={{
-          marginLeft: '20px',
-          width: '25%',
-        }}
-      />
+      <div className={style.searchWrap}>
+        <Search
+          placeholder="牛皮程序员修炼指南？"
+          onSearch={onSearch}
+          enterButton
+          style={{
+            marginLeft: '20px',
+            width: '25%'
+          }}
+        />
+      </div>
+      <div onClick={handleLoginClick}>
+        <Avatar
+          className={style.avatar}
+          shape="square"
+          icon={<UserOutlined />}
+        />
+      </div>
     </div>
   )
 }
