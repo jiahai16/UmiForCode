58c5ce0b0c93876e7800f76fe5568ffed4939657
LZY_Shrio <170280176@qq.com>
2022-01-09 16:36:15 +0800

    feat：登录页面更新

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,15 +1,36 @@
 import { history } from 'umi'
+import { Layout } from 'antd'
+
+const { Header, Footer, Content } = Layout
 import style from './index.less'
 
 export default function LoginPage() {
+  const handleGoHome = () => {
+    history.push('/overview')
+  }
   const insertValue = () => {
     localStorage.setItem('login', 'true')
     history.push('/overview')
   }
   return (
-    <div>
-      <div> 请在localStorage中添加login：true</div>
-      <button onClick={insertValue}>点击注入灵魂即可登录！</button>
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Header>
+        <div className={style.siderHeader} onClick={handleGoHome}></div>
+      </Header>
+      <Content>
+        <div className={style.wrap}>
+          <h1>程序员成长榨汁机🍋</h1>
+          <p>扫码登录即刻加入技术探讨！</p>
+          <div className={style.codeWrap}>
+            <h1 style={{ color: 'white' }}>这是一个二维码</h1>
+          </div>
+          <div> 请在localStorage中添加login：true</div>
+          <button onClick={insertValue}>点击注入灵魂即可登录！</button>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        <a href="#">KKO</a> Design ©2021 Power by XJH
+      </Footer>
+    </Layout>
   )
 }
