41c97bee7b404ed3ec7db6a1dbbfb14abd973b25
LZY_Shrio <170280176@qq.com>
2022-02-25 14:02:42 +0800

    feat：页面标签渲染

--- src/pages/overview/UserLabel/index.tsx
+++ src/pages/overview/UserLabel/index.tsx
@@ -1,8 +1,20 @@
-import { useState } from 'react'
+import { ReactNode, useState } from 'react'
 import { Avatar, Tag, Input, Tooltip, Button } from 'antd'
 import { UserOutlined, PlusOutlined } from '@ant-design/icons'
 import style from './index.less'
 import UserUpdataModal from 'overview/UserUpdataModal'
+
+const tagData = ['标签1', '测试标签']
+const tagColor = [
+  'magenta',
+  'red',
+  'volcano',
+  'orange',
+  'gold',
+  'green',
+  'geekblue',
+  'purple'
+]
 
 export default function UserLable() {
   const [isUpdataModalVisible, setIsUpdataModalVisible] =
@@ -20,6 +32,12 @@ export default function UserLable() {
     setIsUpdataModalVisible(false)
   }
 
+  const randerTags = (data: string[]) => {
+    return data.map((e) => (
+      <Tag color={tagColor[Math.floor(Math.random() * 8)]}>{e}</Tag>
+    ))
+  }
+
   return (
     <div className={style.userlable}>
       <div className={style.userlableLeft}>
@@ -28,11 +46,15 @@ export default function UserLable() {
           <h3>Noob-Builder-JD</h3>
           <p>Level : 301</p>
           <p>Job : null</p>
-          <p>Tag : <span>123</span></p>
+          <p>
+            Tag : <span>{randerTags(tagData)}</span>
+          </p>
         </div>
       </div>
       <div className={style.userlableRight}>
-        <Button type="dashed" onClick={handleClickUpdata}>编辑资料</Button>
+        <Button type="dashed" onClick={handleClickUpdata}>
+          编辑资料
+        </Button>
       </div>
       <UserUpdataModal
         visible={isUpdataModalVisible}
