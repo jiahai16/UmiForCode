f8110ccfed8f2a3563ab55972b05d3ee840e822c
LZY_Shrio <170280176@qq.com>
2022-02-27 19:55:22 +0800

    feat：登录注册接口

---
+++ src/services/user.ts
@@ -0,0 +1,30 @@
+import { request } from 'umi'
+
+type loginBody = {
+  account: string
+  password: string
+  email?: string
+}
+type registerBody = {
+  account: string
+  password: string
+  email: string
+  code: string
+  img: string
+}
+
+export const signInFunc = (data: loginBody) => {
+  return request('/core-api/user/login', { method: 'post', data })
+}
+
+export const signUpFunc = (data: registerBody) => {
+  return request('/core-api/user/register', { method: 'post', data })
+}
+
+export const sendEmailFunc = (data: string) => {
+  return request('/core-api/user/send-email', { method: 'post', data })
+}
+
+export const changePassword = (data: string) => {
+  return request('/core-api/user/send-email', { method: 'post', data })
+}
